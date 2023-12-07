a88a721b166c861ab15fdea63d3a402603f573db
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2023-12-07 13:06:38 +0200

    Week 6 exercise 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
-from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+from query_builder import QueryBuilder
+from statistics import Statistics
 
 
 def main():
@@ -8,13 +8,14 @@ def main():
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
+    query = QueryBuilder()
+
+    matcher = (
+        query
+        .plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
     )
 
     for player in stats.matches(matcher):
